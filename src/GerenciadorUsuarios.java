f578cd58cfaf78ceadc80409d302b09db4465505
Cleuber Eduardo <cleubereduardo52@gmail.com>
2026-04-05 11:59:45 -0300

    Envio do código para o GitHub

---
+++ src/GerenciadorUsuarios.java
@@ -0,0 +1,76 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class GerenciadorUsuarios {
+    
+
+    //ArrayList para guardar usuarios 
+    private List<Usuario> listaUsuarios = new ArrayList<>();
+
+
+    //Adicionar um usuario na arrayList
+    public void adicionarUsuarioList(Usuario usuario){
+        this.listaUsuarios.add(usuario);
+    }
+
+    //remove um usuario de arraylist
+    public void removerUsuarioList(Usuario usuario){
+        this.listaUsuarios.remove(usuario);
+    }
+
+
+
+    //Criar um usuario por meio do metodo
+    public void criarUsuario(String nome, String cpf, String email){
+        Usuario usuario = new Usuario(nome, cpf, email);
+        adicionarUsuarioList(usuario);
+
+        System.out.println("Usuario criado com sucesso");
+    }
+
+    //Metodo responsavel por mostar todos os Usuarios presentes na lista
+    public void mostrarUsuarios(){
+       for(Usuario u : listaUsuarios){
+            System.out.println("== Usuario ==");
+            System.out.printf("ID: %s\n", u.getId());
+            System.out.printf("Nome: %s\n", u.getNome());
+            System.out.printf("CPF: %s\n", u.getCpf());
+            System.out.printf("E-mail: %s\n", u.getEmail());
+            System.out.println();
+       }
+    }
+
+    public Usuario buscarUsuario(int idBusca){
+        for(Usuario u : listaUsuarios){
+            if(u.getId() == idBusca) {
+              return u;
+            }
+        }
+        return null;
+    }
+
+
+   /* System.out.printf("== Usuario %s ==\n", idBusca);
+                System.out.printf("ID: %s\n", u.getId());
+                System.out.printf("Nome: %s\n", u.getNome());
+                System.out.printf("CPF: %s\n", u.getCpf());
+                System.out.printf("E-mail: %s\n", u.getEmail());
+                System.out.println();
+    */
+
+    //Metodo completo que recebe um ID do usuario e procura ele ao longo da lista
+    public void removerUsuario(){
+        int idBusca = 0;
+        Usuario encontrado = buscarUsuario(idBusca);
+        //usa a Variavel que recebeu o número do ID
+        if(encontrado != null){
+            //Usa o metodo criado para remover um usuario especifico
+            removerUsuarioList(encontrado);
+        }
+        else{
+            System.out.println("Usuario não encontrado");
+        }
+    }
+
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,53 @@
+public class Usuario {
+
+    //Atributos do usuário
+    private String nome;
+    private String cpf;
+    private String email;
+    //Usando o static porque é um contador geral da classe e não de um objeto.
+    //Isso é para que não corra o risco de ter Ids duplicados.
+    private static int contadorId = 1;
+    private int id;
+
+
+    //construtor usuario
+    public Usuario(String nome, String cpf, String email){
+        this.id = contadorId;
+        contadorId++;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    // metodos gets
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    //metodos sets
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
